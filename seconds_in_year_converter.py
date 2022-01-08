d4367427705337e21da5aa4bd353e0aafb4d72fd
Benjamin Owusu <90052579+Storerun@users.noreply.github.com>
2022-01-08 22:38:41 +0000

    Add files via upload

---
+++ seconds_in_year_converter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Seconds in a year App
+# This app calculates the number of seconds in a given year.
+
+# In[2]:
+
+
+year= int(input('Enter year: '))
+seconds= year * 31536000
+print('The number of seconds is {}.'.format(seconds))
+
+
+# In[ ]:
+
+
+
+
